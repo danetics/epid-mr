66cce038a7c9c7f2f285a63b93d3a0763578efab
Dan Wright <djw98@epidmac05.local>
2023-07-27 18:05:17 +0100

    improve docstrings

--- mr/steig.py
+++ mr/steig.py
@@ -1,37 +1,38 @@
 '''
 Functions to calculate Steiger-MR Z scores and define Steiger failures
-Logic is based on original Hemani 2017 paper, plus associated R package
 '''
 
 import pandas as pd
 import numpy as np
 from scipy.stats import norm, f
 
-# NOTE: Steiger calculations are dependent on per variant pval aone, which makes them sensitive to pval type precision
+# Steiger calculations are dependent on per variant pval aone, which makes them sensitive to pval type precision
+# Logic here is based on original Hemani 2017 paper and the worked example from that publication (code lines are permalinked)
+# Equivalence between R and scipy.stats distribution function from https://albertotb.com/Equivalence-between-distribution-functions-in-R-and-Python/
 
 def calc_f_from_sumstats(pval: pd.Series, 
                          n: pd.Series) -> pd.Series:
     ''' 
-    Calculate F-statistic of a variant from pval and n in sumstats
-    using the f distribution inverse survival function
-    as here https://github.com/explodecomputer/causal-directions/blob/916f528cbf8d1ccd9bc7eac9f33f6b658e683ded/scripts/mr_directionality_analysis.R#L39
+    Calculate F-statistic of a variant from pval and n in sumstats using the f distribution inverse survival function
     '''
-    return f.isf(pval, 1, n-1)
+    # https://github.com/explodecomputer/causal-directions/blob/916f528cbf8d1ccd9bc7eac9f33f6b658e683ded/scripts/mr_directionality_analysis.R#L39
+    # f.isf == R qf(lower.tail=FALSE)
+    return f.isf(pval, 1, n-1) # one sided 
 
 def calc_r_from_f(f: pd.Series, 
                   n: pd.Series) -> pd.Series:
     ''' 
-    Convert F to correlation r (unsquared) using accepted approach 
-    as here https://github.com/explodecomputer/causal-directions/blob/916f528cbf8d1ccd9bc7eac9f33f6b658e683ded/scripts/mr_directionality_analysis.R#L40
-    and here http://davidileitman.com/wp-content/uploads/2014/04/EffectSizeFormulas.pdf
+    Convert F to correlation r (unsquared) using accepted approach with 1 DF for the numerator
     '''
+    # https://github.com/explodecomputer/causal-directions/blob/916f528cbf8d1ccd9bc7eac9f33f6b658e683ded/scripts/mr_directionality_analysis.R#L40
+    # http://davidileitman.com/wp-content/uploads/2014/04/EffectSizeFormulas.pdf
     return np.sqrt(f/(f+(n-2)))
 
 def transform_r_to_zg(r: pd.Series) -> pd.Series:
     ''' 
     Use Fisher transformation to convert per-variant r to per-variant Zg
-    from Hemani 2017, p16, equation 2
     '''
+    # Hemani 2017, p16, equation 2
     return 0.5 * np.log((1+r)/(1-r)) # np.log is ln() 
 
 def calc_steiger_z(zgx: pd.Series, 
@@ -40,26 +41,27 @@ def calc_steiger_z(zgx: pd.Series,
                    ny: pd.Series) -> pd.Series:
     ''' 
     Calculate per-variant Steiger's Z using formula for 2SMR 
-    from Hemani 2017, p17, equation 1
     '''
+    # Hemani 2017, p17, equation 1
     return (zgx-zgy)/np.sqrt((1/(nx-3)) + (1/(ny-3)))
 
 def calc_ratio_pval(sey, bx, ratio):
     '''
     Calculate wald ratio p-value for the by/bx ratio
     This is the per-variant p-value for a single-variant MR using that variant as the instrument
-    from https://github.com/explodecomputer/causal-directions/blob/916f528cbf8d1ccd9bc7eac9f33f6b658e683ded/scripts/mr_directionality_analysis.R#L143
     '''
+    # https://github.com/explodecomputer/causal-directions/blob/916f528cbf8d1ccd9bc7eac9f33f6b658e683ded/scripts/mr_directionality_analysis.R#L143
+    # norm.sf == R pnorm(lower.tail=FALSE)
     wald_se = sey/abs(bx)
     wald_z = abs(ratio)/wald_se # ratio already pre-calculated in input
-    return (2 * norm.sf(wald_z))
+    return (2 * norm.sf(wald_z)) # two-sided
     
 def get_failures(data, steigerz, waldp):
     '''
     Subset per-variant failures, where we accept y->x causation based on sign(steigerz) and the steigerz p-value
-    from logic in Hemani 2017, p16
     '''
-    steigerp = 2 * norm.sf(steigerz) # Steiger's Z is normally distributed
+    # Hemani 2017, p16
+    steigerp = 2 * norm.sf(steigerz)
     return data[((steigerp < 0.05) & (steigerz < 0) & (waldp < 0.05))]
 
 def apply_steiger(data: pd.DataFrame) -> tuple:
